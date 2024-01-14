181e40b53f8c5b191ed6b82cd9bce742e9d83607
zouyifan <ft1216@foxmail.com>
2024-01-14 17:25:00 -0600

    feat: add server startup and shutdown notice

--- true-love-server/main.py
+++ true-love-server/main.py
@@ -1,9 +1,27 @@
+import signal
+
+import base_client
 import job_mgmt
 import server
 from configuration import Config
 
 
+def notice_master():
+    # 启动的通知
+    base_client.send_text(config.BASE_SERVER.get("master_wxid"), "", "真爱粉server启动成功...")
+
+    # 设置信号被杀的回调
+    def handler(sig, frame):
+        # 退出前清理环境
+        base_client.send_text(config.BASE_SERVER.get("master_wxid"), "", "真爱粉server正在关闭...")
+        exit(0)
+
+    signal.signal(signal.SIGINT, handler)
+
+
 def main():
+    # notice master
+    notice_master()
     # 注册并且 异步启动定时任务
     job = job_mgmt.Job()
     job.async_enable_jobs()
@@ -12,5 +30,5 @@ def main():
 
 
 if __name__ == '__main__':
-    Config()
+    config = Config()
     main()
