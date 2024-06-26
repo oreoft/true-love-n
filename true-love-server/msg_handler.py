77be2ba61c242ac3d8af7e65bac8ce3f3776f22c
zouyifan <ft1216@foxmail.com>
2024-06-26 10:36:06 +0800

    feat(server): modfiy the gen trig

--- true-love-server/msg_handler.py
+++ true-love-server/msg_handler.py
@@ -44,7 +44,7 @@ class MsgHandler:
             return "你@我干嘛？"
 
         # 如果是生图
-        if q.startswith('生成') and '图片' in q:
+        if q.startswith('生成') and '片' in q:
             self.LOG.info(f"收到:{msg.sender}, 生成图片:{q}")
             return handler.gen_img(q, (msg.roomid if msg.from_group() else msg.sender), msg.sender)
 
