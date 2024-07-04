d904a19ddf78904ec80ff4ccf4f1360a0b14d3f0
zouyifan <ft1216@foxmail.com>
2024-07-04 14:53:07 +0800

    feat(base): quote type add the img

--- true-love-server/chatgpt.py
+++ true-love-server/chatgpt.py
@@ -77,6 +77,7 @@ class ChatGPT(ChatBot):
             # 发送请求
             response = requests.post(url, headers=headers, data=json.dumps(data))
             self.LOG.info(response.json())
+            self.LOG.info(response.text)
             # 获取结果
             rsp = response.json().get('data') or response.json().get('message')
         except Exception as e0:
