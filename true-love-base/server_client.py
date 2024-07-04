c0a104f2f31d279b72a8d4bcdb314a0368b62dc3
zouyifan <ft1216@foxmail.com>
2024-07-04 13:17:48 +0800

    feat(base): quote type add the img

--- true-love-base/server_client.py
+++ true-love-base/server_client.py
@@ -38,7 +38,8 @@ def get_chat(req: WxMsg, wcf: Wcf):
             # 使用正则表达式搜索
             match = re.search(pattern, req.content)
             LOG.info("match:%", match)
-            LOG.info("extra:%s", CACHE)
+            LOG.info("match.group(1):%", match.group(1))
+            LOG.info("extra:%s", CACHE.get(match.group(1)))
             if match and CACHE.get(match.group(1)):
                 base64_string = image_to_base64(
                     wcf.download_image(id=int(match.group(1)), extra=CACHE.get(match.group(1)), dir=save_img_dir,
