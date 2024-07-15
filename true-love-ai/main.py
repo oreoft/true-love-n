2b9a492bcf0ba6cc17a3b949375b27064a0b6067
zouyifan <ft1216@foxmail.com>
2024-07-15 11:53:48 +0800

    feat(ai): add the img analyze

--- true-love-ai/main.py
+++ true-love-ai/main.py
@@ -7,12 +7,12 @@ from configuration import Config
 
 def notice_master():
     # 启动的通知
-    base_client.send_text("master", "", "真爱粉server启动成功...")
+    base_client.send_text("master", "", "真爱粉ai启动成功...")
 
     # 设置信号被杀的回调
     def handler(sig, frame):
         # 退出前清理环境
-        base_client.send_text("master", "", "真爱粉server正在关闭...")
+        base_client.send_text("master", "", "真爱粉ai正在关闭...")
         exit(0)
 
     signal.signal(signal.SIGINT, handler)
