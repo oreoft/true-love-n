d9777f9bb6e70cc704a34454958ff15e0f4a233d
zouyifan <meetyifan@gmail.com>
2026-01-21 10:48:32 -0600

    fix: update wxautox4 dependency to version 40.1.8 and comment out message reply in group chat

--- true-love-base/src/true_love_base/core/wxauto_client.py
+++ true-love-base/src/true_love_base/core/wxauto_client.py
@@ -230,7 +230,7 @@ class WxAutoClient():
                 # 判断content是否超过 15行
                 if is_group and content.count('\n') > 15:
                     # 随便回复点二次元内容，把消息顶上去
-                    res = self.wx.SendMsg("(｡･ω･｡)ﾉ♡", chat_name)
+                    # res = self.wx.SendMsg("(｡･ω･｡)ﾉ♡", chat_name)
                     LOG.info("Replied to long message in group to push it up: %r", res)
 
                 # 转换消息
