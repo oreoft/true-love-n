5fb507105ef18d3c13a6c19f55504776736052c0
zouyifan <meetyifan@gmail.com>
2026-04-19 18:27:58 -0500

    refactor: remove unused search service and related fetch function from exports

--- true-love-ai/src/true_love_ai/services/__init__.py
+++ true-love-ai/src/true_love_ai/services/__init__.py
@@ -4,13 +4,11 @@
 服务模块
 提供聊天、图像、搜索等服务
 """
+
 from true_love_ai.services.chat_service import ChatService
 from true_love_ai.services.image_service import ImageService
-from true_love_ai.services.search_service import SearchService, fetch_baidu_references
 
 __all__ = [
     'ChatService',
     'ImageService',
-    'SearchService',
-    'fetch_baidu_references',
 ]
